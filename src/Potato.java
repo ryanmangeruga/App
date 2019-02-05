51dcb64e6be742a2922ce4b2cab4e9b147e7d837
ryanmangeruga <47357026+ryanmangeruga@users.noreply.github.com>
2019-02-05 09:40:37 -0500

    Update Potato.java

--- src/Potato.java
+++ src/Potato.java
@@ -1,5 +1,6 @@
 public class Potato {
     public static void main(String[] args){
         System.out.println("Hola, como estas");
+        System.out.println("Es importante proteger el medio ambiente. ");
     }
 }
