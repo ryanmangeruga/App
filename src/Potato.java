2ffc5bee6d775cf32aaee821de3fd2aa1c90377f
ryanmangeruga <rmangeruga4@gmail.com>
2019-02-05 09:47:29 -0500

    Second commit

--- src/Potato.java
+++ src/Potato.java
@@ -2,5 +2,6 @@ public class Potato {
     public static void main(String[] args){
         System.out.println("Hola, como estas");
         System.out.println("Es importante proteger el medio ambiente. ");
+        System.out.println("My iphone burned my hand! ");
     }
 }
